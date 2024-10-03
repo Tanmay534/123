7d77cd4d8d24df4a89a4fdd45d990e3261abf4dc
Tanmay534 <mohodtanmay1@gmail.com>
2024-10-03 19:20:38 +0530

    update2.0

--- Test111/src/Main1.java
+++ Test111/src/Main1.java
@@ -1,6 +1,7 @@
 
 public class Main1 {
-public static void main(String[] args) {
-	System.out.println("update");
-}
+	public static void main(String[] args) {
+		System.out.println("update");
+		System.out.println("update12.0");
+	}
 }
